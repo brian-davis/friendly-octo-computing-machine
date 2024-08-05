196808749f1b6a7b6861dd4d48c7007d9294ef17
Brian Davis <gbriandavis@fastmail.com>
2024-08-05 15:41:06 -0700

    improve left column appearance

--- app/javascript/controllers/works_controller.js
+++ app/javascript/controllers/works_controller.js
@@ -26,5 +26,7 @@ export default class extends Controller {
         Accept: "text/vnd.turbo-stream.html, text/html, application/xhtml+xml",
       },
     });
+
+    event.currentTarget.value = "";
   }
 }
