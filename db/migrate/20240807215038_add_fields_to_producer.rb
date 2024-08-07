a8354fab28ff251a797d8061391c0fcefb8c3b47
Brian Davis <gbriandavis@fastmail.com>
2024-08-07 15:14:05 -0700

    add fields to producer

---
+++ db/migrate/20240807215038_add_fields_to_producer.rb
@@ -0,0 +1,9 @@
+class AddFieldsToProducer < ActiveRecord::Migration[7.1]
+  def change
+    add_column :producers, :birth_year, :integer
+    add_column :producers, :death_year, :integer
+    add_column :producers, :bio_link, :string
+    add_column :producers, :nationality, :string
+    add_column :producers, :language, :string
+  end
+end
