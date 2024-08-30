9b3b0b8ea01c0dbc53f8d78708e90fb5ec2f6b38
Brian Davis <gbriandavis@fastmail.com>
2024-08-29 17:40:48 -0700

    flex-wrap css for book tags

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # allow LAN access
+  # $ ifconfig | grep broadcast
+  # $ bin/rails s -b 0.0.0.0 -p 3000
+  # config.hosts << 'hostname.local'
+  config.hosts.clear
 end
