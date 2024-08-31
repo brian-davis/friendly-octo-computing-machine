3543c24429e2cec7c3c03b8cd466ef8bda624cc9
Brian Davis <gbriandavis@fastmail.com>
2024-08-31 14:13:16 -0700

    fix reading_sessions tests and fixtures

--- test/controllers/reading_sessions_controller_test.rb
+++ test/controllers/reading_sessions_controller_test.rb
@@ -2,47 +2,48 @@ require "test_helper"
 
 class ReadingSessionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @reading_session = reading_sessions(:one)
+    @work = works(:one)
+    @reading_session = reading_sessions(:one) # references @work
   end
 
   test "should get index" do
-    get reading_sessions_url
+    get work_reading_sessions_url(@work)
     assert_response :success
   end
 
   test "should get new" do
-    get new_reading_session_url
+    get new_work_reading_session_url(@work)
     assert_response :success
   end
 
   test "should create reading_session" do
     assert_difference("ReadingSession.count") do
-      post reading_sessions_url, params: { reading_session: { ended_at: @reading_session.ended_at, pages: @reading_session.pages, started_at: @reading_session.started_at, work_id: @reading_session.work_id } }
+      post work_reading_sessions_url(@work), params: { reading_session: { ended_at: @reading_session.ended_at, pages: @reading_session.pages, started_at: @reading_session.started_at, work_id: @reading_session.work_id } }
     end
 
-    assert_redirected_to reading_session_url(ReadingSession.last)
+    assert_redirected_to work_reading_session_url(@work, ReadingSession.last)
   end
 
   test "should show reading_session" do
-    get reading_session_url(@reading_session)
+    get work_reading_session_url(@work, @reading_session)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_reading_session_url(@reading_session)
+    get edit_work_reading_session_url(@work, @reading_session)
     assert_response :success
   end
 
   test "should update reading_session" do
-    patch reading_session_url(@reading_session), params: { reading_session: { ended_at: @reading_session.ended_at, pages: @reading_session.pages, started_at: @reading_session.started_at, work_id: @reading_session.work_id } }
-    assert_redirected_to reading_session_url(@reading_session)
+    patch work_reading_session_url(@work, @reading_session), params: { reading_session: { ended_at: @reading_session.ended_at, pages: @reading_session.pages, started_at: @reading_session.started_at, work_id: @reading_session.work_id } }
+    assert_redirected_to work_reading_session_url(@work, @reading_session)
   end
 
   test "should destroy reading_session" do
     assert_difference("ReadingSession.count", -1) do
-      delete reading_session_url(@reading_session)
+      delete work_reading_session_url(@work, @reading_session)
     end
 
-    assert_redirected_to reading_sessions_url
+    assert_redirected_to work_reading_sessions_url(@work)
   end
 end
